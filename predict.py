1fef113b15db6096b2cefe74b0935ec80665fa13
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2023-08-01 16:30:32 +0800

    now working with vid capture

--- predict.py
+++ predict.py
@@ -1,16 +1,27 @@
 from ultralytics import YOLO
 
 import cv2
+cam = cv2.VideoCapture(1)
+print(cam)
 
 # Load a model
 # model = YOLO('yolov8n.pt')  # load an official model
 model = YOLO('runs/detect/train/weights/best.pt')  # load a custom model
-img = cv2.imread("bus.jpg")
-results = model.predict(img)
 
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
 
-# res = model(img)
-res_plotted = results[0].plot()
-cv2.imshow("result", res_plotted)
-cv2.waitKey()
+    results = model(frame, device='mps')
+    res_plotted = results[0].plot()
+    cv2.imshow("result", res_plotted)
+    
+    if cv2.waitKey(1) == ord('q'):
+        print("q pressed")
+        break
+
+cam.release()
+cv2.destroyAllWindows()
 
